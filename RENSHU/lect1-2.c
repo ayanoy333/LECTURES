d403d52d8d6df5a2a165393d3402ff56db99cc09
ayano333 <kknami2wave@gmail.com>
2023-01-10 01:01:43 +0900

    kyou

---
+++ RENSHU/lect1-2.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main (int argc,char**argv)
+{
+    int a;
+    a = 100;
+    printf("a=%d\r\n",a);
+}
--- lecture18/createtest.cpp
+++ lecture18/createtest.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>                    //cout
 #include <pqxx/pqxx> 
 
 using namespace std;
