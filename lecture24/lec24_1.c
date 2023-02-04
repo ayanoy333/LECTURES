43fb6e36c6c022411751b71d68ae095c9e5e5d81
Takahiro Oku <pigment@mac.com>
2023-02-04 18:12:44 +0900

    char ではなく unsigned charにします

--- lecture24/lec24_1.c
+++ lecture24/lec24_1.c
@@ -4,12 +4,12 @@
 int main(void)
 {
     int i,j;
-    char c[10][10];
+    unsigned char c[10][10];
 
     // 表示部
     for (j=0;j<10;j++){
         for(i=0;i<10;i++){
-            printf("%x ",c[j][i]);
+            printf("%02x ",c[j][i]);
         }
         printf("\r\n");
     }
