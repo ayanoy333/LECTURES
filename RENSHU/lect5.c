a8c46baf2e158d9250c5db255de073660c6408c0
ayano333 <kknami2wave@gmail.com>
2023-02-04 17:00:33 +0900

    kinou

---
+++ RENSHU/lect5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "rnd1.h"
+#include "misc1.h"
+
+int main (int argc,char**argv)
+{
+    char input[80];
+    int rndnum,inputnum,diff,result;
+    while (1)
+    {
+        rndnum = getRnd(0,9);
+        printf("0-9までの範囲で乱数を生成しました\r\n");
+        printf("数を当ててください('q')で終了\r\n");
+        while (1)
+        {
+            printf("→");
+            scanf("%s",input);
+            inputnum = input[0] - 0x30;
+            if (input[0] == 'q')
+            {
+                printf("終了します\r\n");
+                return 0;
+            }
+            result = judge(inputnum,rndnum);
+            dispMessage(result);
+            if (result == EQUAL)
+            {
+                break;
+            }
+        }
+        
+    }
+    
+}
